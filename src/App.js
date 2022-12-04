4edfc2d07060533c201f09e600b1351c572fcdea
unknown <lajja.lamprostech@gmail.com>
2022-12-04 09:09:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import StarRating from "./StarRating";
+import logo from "../src/components/assets/logo.png";
+import "./styles/home.scss";
+import { useState } from "react";
+import company from "../src/components/Data";
+
+import "@rainbow-me/rainbowkit/styles.css";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
 
 function App() {
+  const { chains, provider } = configureChains(
+    [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
+    [
+      alchemyProvider({ apiKey: process.env.REACT_APP_ALCHEMY_ID }),
+      publicProvider(),
+    ]
+  );
+
+  const { connectors } = getDefaultWallets({
+    appName: "Marcat",
+    chains,
+  });
+
+  const wagmiClient = createClient({
+    autoConnect: true,
+    connectors,
+    provider,
+  });
+
+  const [rating2, setRating2] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <div className="App">
+          <div className="navbar">
+            <img className="logo" src={logo} alt="logo" />
+            <div className="navbar-right">
+              <ConnectButton
+                accountStatus={{
+                  smallScreen: "avatar",
+                  largeScreen: "full",
+                }}
+              />
+            </div>
+          </div>
+          <div className="main-section">
+            <h1 className="title">Find the new & upcoming dApps here!</h1>
+            <h1 className="title">
+              Let all the other users know how you like them
+            </h1>
+
+            {company.map((item, key) => {
+              return (
+                <div className="card" key={key}>
+                  <div className="first">
+                    <div className="company-img">
+                      <img
+                        className="company_img"
+                        src={item.logo}
+                        alt="companylogo"
+                      />
+                    </div>
+                    <div className="company-details">
+                      <h2 className="company-name">{item.name}</h2>
+                      <a href={item.link}>{item.link}</a>
+                      <p>About the dApp:</p>
+                      <p>{item.desc}</p>
+                    </div>
+                  </div>
+                  <div className="second">
+                    <div className="left-inside-second">
+                      <span>{rating2} ★</span>
+                      <span>1K+ reviews</span>
+                      <span>DeFi</span>
+                    </div>
+                    <div className="right-inside-second">
+                      <span>Rate this dApp</span>{" "}
+                      <StarRating setRating2={setRating2} />
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </RainbowKitProvider>
+    </WagmiConfig>
   );
 }
 
---
+++ src/components/Data.js
@@ -0,0 +1,33 @@
+import logo1 from "../components/assets/valist.svg";
+import logo2 from "../components/assets/worldcoin_logo_new.png";
+import logo3 from "../components/assets/push.jpg";
+import logo4 from "../components/assets/powerloom.webp";
+
+const data = [
+  {
+    name: "Valist",
+    desc: "Easily ship web, desktop, and mobile software to any platform — all without managing complex tooling, infra, or using traditional DRM systems that take 30%+ of revenue.",
+    link: "https://app.valist.io/-/discover",
+    logo: logo1,
+  },
+  {
+    name: "Worldcoin",
+    desc: "Worldcoin is a new global digital currency that will launch by giving a free share to every human on Earth.",
+    link: "https://developer.worldcoin.org/login",
+    logo: logo2,
+  },
+  {
+    name: "Push Protocol",
+    desc: "Push Protocol is a web3 communication network, enabling cross-chain notifications and messaging for dapps, wallets, and services.",
+    link: "https://push.org/",
+    logo: logo3,
+  },
+  {
+    name: "Power Loom Protocol",
+    desc: "PowerLoom Protocol aggregates on-chain and off-chain data to generate snapshots with cryptographic proofs in a decentralized manner. It aims to build trust and offer meaningful insights on demand through a rich ecosystem of peers and stakeholders that are incentivized to participate in the protocol.",
+    link: "https://powerloom.io/",
+    logo: logo4,
+  },
+];
+
+export default data;
